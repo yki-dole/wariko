fb9da417bb90442bd6dfb6aa7ef92882b741ebe7
yki-dole <poppai@example.com>
2019-09-03 21:12:47 +0900

    change to github

--- main.go
+++ main.go
@@ -138,7 +138,7 @@ func loginHandler(c *gin.Context) {
 	c.Bind(&loginData)
 	result := AccountCheck(loginData.Id, loginData.Pass)
 	if result == 0 {
-		c.Redirect(200, "/signup")
+		c.Redirect(200, "/signin")
 	} else {
 		c.Redirect(200, "/signup")
 	}
